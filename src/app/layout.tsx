980bde4b02ac363e839ec3ccc9b75133a975f974
ChibuzoEkwue <chieezy5@gmail.com>
2025-02-12 18:10:44 +0100

    remove fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Tix",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`grid min-h-svh grid-rows-[auto_1fr_auto] antialiased`}
-      >
+      <body className={`grid min-h-svh grid-rows-[auto_1fr_auto] antialiased`}>
         <Header />
         {children}
         <Footer />
